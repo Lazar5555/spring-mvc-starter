1a297557f76e44d278318d2e3ce1ae5127973e8e
Eleazar Ponce <poncebe@gmail.com>
2018-04-03 21:07:27 -0500

    Refactpring type of freePassed variable on Customer for handle empty field error

--- src/com/eleazar/springdemo/mvc/Customer.java
+++ src/com/eleazar/springdemo/mvc/Customer.java
@@ -14,9 +14,10 @@ public class Customer {
 	@Size(min=1, message="is required")
 	private String lastName;
 	
+	@NotNull(message="is required")
 	@Min(value=0, message="must be greater than or equal to 0")
 	@Max(value=10, message="must be less than or equal to 10")
-	private int freePasses;
+	private Integer freePasses;
 	
 	@Pattern(regexp="^[0-9]{5}", message="Just 5 numbers") // regexp for just numbers and letters: ^[a-zA-Z0-9]{5}
 	private String zipcode;
@@ -33,10 +34,10 @@ public class Customer {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	public int getFreePasses() {
+	public Integer getFreePasses() {
 		return freePasses;
 	}
-	public void setFreePasses(int freePasses) {
+	public void setFreePasses(Integer freePasses) {
 		this.freePasses = freePasses;
 	}
 	public String getZipcode() {
